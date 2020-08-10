5e023d4ac6aca253a130e50d2671af15933f999b
OusmaneMballo <ousmanemballo576@gmail.com>
2020-08-10 10:38:40 +0000

    Configuration des routes

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -3,14 +3,25 @@
 namespace App\Controllers;
 use CodeIgniter\Controller;
 
-class Page
+class Page extends Controller
 {
     public function index()
     {
-        return view('welcome_message');
+        return view('home');
     }
 
     public function view($page = 'home')
     {
+        if ( ! is_file(APPPATH.'/Views/Page/'.$page.'.php'))
+        {
+            // Whoops, we don't have a page for that!
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+        }
+
+        $data['title'] = ucfirst($page); // Capitalize the first letter
+
+        echo view('Templates/header', $data);
+        echo view('Page/'.$page, $data);
+        echo view('Templates/footer', $data);
     }
 }
