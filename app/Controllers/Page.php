44e0c18ac5c45074f3bf2c6d3557af40bf3c3b88
OusmaneMballo <ousmanemballo576@gmail.com>
2020-08-10 09:56:21 +0000

    Mise en place du layout

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -1,10 +1,16 @@
 <?php
 
-
 namespace App\Controllers;
-
+use CodeIgniter\Controller;
 
 class Page
 {
+    public function index()
+    {
+        return view('welcome_message');
+    }
 
+    public function view($page = 'home')
+    {
+    }
 }
